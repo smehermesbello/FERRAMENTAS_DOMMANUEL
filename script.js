349f7d35013ba2a50f771d117920f54d25fde39d
smehermesbello <hermesbello@educacao.curitiba.pr.gov.br>
2026-03-12 13:56:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,12 @@ async function generatePreview() {
     const turnoRadio = document.querySelector('input[name="turno"]:checked');
     const area = document.getElementById('pdf-area');
 
-    if (!input.files.length) return alert("Por favor, selecione as fotos.");
+    if (!input.files.length) return alert("POR FAVOR, SELECIONE AS FOTOS.");
     
     const turno = turnoRadio.value;
+    const classeLinha = (turno === 'manha') ? 'linha-manha' : 'linha-tarde';
+    const classeBordaFoto = (turno === 'manha') ? 'borda-manha' : 'borda-tarde';
+
     await document.fonts.ready;
     area.innerHTML = "";
     showScreen('screen-preview');
@@ -33,12 +36,12 @@ async function generatePreview() {
             
             page.innerHTML += `
                 <div class="etiqueta">
-                    <div class="etiqueta-topo">
+                    <div class="etiqueta-topo ${classeLinha}">
                         <img src="LOGO.jpg" class="etiqueta-logo">
                         <div class="etiqueta-escola">ESCOLA MUNICIPAL DOM MANUEL DA SILVEIRA D’ELBOUX</div>
                     </div>
                     <div class="etiqueta-corpo">
-                        <img src="${src}" class="foto-aluno ${turno === 'manha' ? 'borda-manha' : 'borda-tarde'}">
+                        <img src="${src}" class="foto-aluno ${classeBordaFoto}">
                         <div class="nome-aluno" contenteditable="true">${nome}</div>
                     </div>
                 </div>`;
